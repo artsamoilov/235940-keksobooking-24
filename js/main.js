51673aea69a601f4da3d8305678a5e42786faf05
Artur Samoilov <artsamoilov@gmail.com>
2021-09-25 18:41:02 +0300

    убирает лишнюю функцию

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-const getFloatFromRange = (min, max) => max > min ? Math.random() * (max - min) + min : Math.random() * (min - max) + max;
+const getFloatFromRange = (min, max, precision) => {
+  const floatNumber = max > min ? Math.random() * (max - min) + min : Math.random() * (min - max) + max;
+  return Number(floatNumber.toFixed(precision));
+};
 
-const getIntegerFromRange = (min, max) => Math.round(getFloatFromRange(min, max));
+const getIntegerFromRange = (min, max) => getFloatFromRange(min, max, 0);
 
-const getFormattedFloatFromRange = (min, max, floatLength) => Number(getFloatFromRange(min, max).toFixed(floatLength));
-
-console.log(getIntegerFromRange(0, 10));
-console.log(typeof getIntegerFromRange(0, 10));
-console.log(getFormattedFloatFromRange(0.1, 1.1, 3));
-console.log(typeof getFormattedFloatFromRange(1.0, 1.1, 3));
+getIntegerFromRange(9, 10);
+getFloatFromRange(1.0, 1.1, 3);
