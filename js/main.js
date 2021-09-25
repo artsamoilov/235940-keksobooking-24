da69dfcd8ee5149c79518c3a2848131d58543320
Artur Samoilov <artsamoilov@gmail.com>
2021-09-25 18:12:15 +0300

    заменяет все функции на стрелочные

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-function getFloatFromRange(min, max) {
-  return max > min ? Math.random() * (max - min) + min : Math.random() * (min - max) + max;
-}
+const getFloatFromRange = (min, max) => max > min ? Math.random() * (max - min) + min : Math.random() * (min - max) + max;
 
-function getIntegerFromRange(min, max) {
-  return Math.round(getFloatFromRange(min, max));
-}
+const getIntegerFromRange = (min, max) => Math.round(getFloatFromRange(min, max));
 
-function getFormattedFloatFromRange(min, max, floatLength) {
-  return Number(getFloatFromRange(min, max).toFixed(floatLength));
-}
+const getFormattedFloatFromRange = (min, max, floatLength) => Number(getFloatFromRange(min, max).toFixed(floatLength));
 
 console.log(getIntegerFromRange(0, 10));
 console.log(typeof getIntegerFromRange(0, 10));
-console.log(getFormattedFloatFromRange(1.0, 1.1, 3));
+console.log(getFormattedFloatFromRange(0.1, 1.1, 3));
 console.log(typeof getFormattedFloatFromRange(1.0, 1.1, 3));
