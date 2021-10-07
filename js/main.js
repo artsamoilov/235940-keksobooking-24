4b7d19be7d0a6704b5aea2d44edfcd2caaa24325
Artur Samoilov <artsamoilov@gmail.com>
2021-10-07 22:26:14 +0300

    переносит константу в main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import {HOUSING_QUANTITY, createHousing} from './utils/data.js';
+import {createHousingOffers} from './utils/data.js';
 
-const housingOffers = Array.from({length: HOUSING_QUANTITY}, createHousing);
+const HOUSING_QUANTITY = 10;
+const housingOffers = createHousingOffers(HOUSING_QUANTITY);
 
 // чтобы линтер не ругался на неиспользуемую переменную
 housingOffers.sort();
--- js/utils/data.js
+++ js/utils/data.js
@@ -1,4 +1,4 @@
-import {getFloatFromRange, getIntegerFromRange} from './random.js';
+import {getFloatFromRange, getIntegerFromRange} from './utils.js';
 
 const HOUSING_TYPES = [
   'palace',
@@ -45,39 +45,41 @@ const DESCRIPTIONS = [
   'Тишина кругом.\nПроникают в сердце скал\nГолоса цикад.',
 ];
 
-const HOUSING_QUANTITY = 10;
+const createHousingOffers = (quantity) => {
+  const indexes = Array.from({length: quantity}, (empty, value) => ++value).sort(() => 0.5 - Math.random());
 
-const indexes = Array.from({length: HOUSING_QUANTITY}, (empty, value) => ++value).sort(() => 0.5 - Math.random());
+  const createHousing = () => {
+    const housingLocation = [getFloatFromRange(35.65, 35.7, 5), getFloatFromRange(139.7, 139.8, 5)];
+    const generateIndex = () => {
+      const index = indexes.splice(getIntegerFromRange(0, indexes.length - 1), 1);
+      return (index < 10) ? `0${index}` : index;
+    };
 
-const createHousing = () => {
-  const housingLocation = [getFloatFromRange(35.65, 35.7, 5), getFloatFromRange(139.7, 139.8, 5)];
-  const generateIndex = () => {
-    const index = indexes.splice(getIntegerFromRange(0, indexes.length - 1), 1);
-    return (index < 10) ? `0${index}` : index;
+    return {
+      author: {
+        avatar: `img/avatars/user${generateIndex()}.png`,
+      },
+      offer: {
+        title: TITLES[getIntegerFromRange(0, TITLES.length - 1)],
+        address: housingLocation.join(', '),
+        price: getIntegerFromRange(1000, 100000),
+        type: HOUSING_TYPES[getIntegerFromRange(0, HOUSING_TYPES.length - 1)],
+        rooms: getIntegerFromRange(1, 10),
+        guests: getIntegerFromRange(1, 10),
+        checkin: CHECK_VARIANTS[getIntegerFromRange(0, CHECK_VARIANTS.length - 1)],
+        checkout: CHECK_VARIANTS[getIntegerFromRange(0, CHECK_VARIANTS.length - 1)],
+        features: FEATURES.slice().sort(() => 0.5 - Math.random()).slice(0, getIntegerFromRange(1, FEATURES.length - 1)),
+        description: DESCRIPTIONS[getIntegerFromRange(0, DESCRIPTIONS.length - 1)],
+        photos: PHOTOS.slice().sort(() => 0.5 - Math.random()).slice(0, getIntegerFromRange(1, PHOTOS.length - 1)),
+      },
+      location: {
+        lat: housingLocation[0],
+        lng: housingLocation[1],
+      },
+    };
   };
 
-  return {
-    author: {
-      avatar: `img/avatars/user${generateIndex()}.png`,
-    },
-    offer: {
-      title: TITLES[getIntegerFromRange(0, TITLES.length - 1)],
-      address: housingLocation.join(', '),
-      price: getIntegerFromRange(1000, 100000),
-      type: HOUSING_TYPES[getIntegerFromRange(0, HOUSING_TYPES.length - 1)],
-      rooms: getIntegerFromRange(1, 10),
-      guests: getIntegerFromRange(1, 10),
-      checkin: CHECK_VARIANTS[getIntegerFromRange(0, CHECK_VARIANTS.length - 1)],
-      checkout: CHECK_VARIANTS[getIntegerFromRange(0, CHECK_VARIANTS.length - 1)],
-      features: FEATURES.slice().sort(() => 0.5 - Math.random()).slice(0, getIntegerFromRange(1, FEATURES.length - 1)),
-      description: DESCRIPTIONS[getIntegerFromRange(0, DESCRIPTIONS.length - 1)],
-      photos: PHOTOS.slice().sort(() => 0.5 - Math.random()).slice(0, getIntegerFromRange(1, PHOTOS.length - 1)),
-    },
-    location: {
-      lat: housingLocation[0],
-      lng: housingLocation[1],
-    },
-  };
+  return Array.from({length: quantity}, createHousing);
 };
 
-export {HOUSING_QUANTITY, createHousing};
+export {createHousingOffers};
