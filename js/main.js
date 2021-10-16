e07bd4807c817ea21c8fee2c2a029dfc4c997ca5
Artur Samoilov <artsamoilov@gmail.com>
2021-10-16 19:32:10 +0300

    меняет рандомный выбор объявления для попапа на первое

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import {createHousingOffers} from './utils/data.js';
 import {createPopup} from './utils/popup.js';
-import {getIntegerFromRange} from './utils/utils.js';
 
 const HOUSING_QUANTITY = 10;
 const housingOffers = createHousingOffers(HOUSING_QUANTITY);
 
-createPopup(housingOffers[getIntegerFromRange(0, HOUSING_QUANTITY - 1)]);
+createPopup(housingOffers[0]);
