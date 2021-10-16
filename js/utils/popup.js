3145f753dded498e67c9c7c2813b887b7d278c8a
Artur Samoilov <artsamoilov@gmail.com>
2021-10-17 01:47:52 +0300

    деструктурирует параметры функции создания попапа

--- js/utils/popup.js
+++ js/utils/popup.js
@@ -4,21 +4,21 @@ const addExistingTextContent = (node, text) => text ? node.textContent = text : 
 
 const addExistingSource = (node, source) => source ? node.src = source : node.classList.add('hidden');
 
-const createPopup = (housing) => {
+const createPopup = ({author: {avatar}, offer: {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos}}) => {
   const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 
-  addExistingSource(cardTemplate.querySelector('.popup__avatar'), housing.author.avatar);
-  addExistingTextContent(cardTemplate.querySelector('.popup__title'), housing.offer.title);
-  addExistingTextContent(cardTemplate.querySelector('.popup__text--address'), housing.offer.address);
-  addExistingTextContent(cardTemplate.querySelector('.popup__text--price'), `${housing.offer.price} ₽/ночь`);
-  addExistingTextContent(cardTemplate.querySelector('.popup__type'), HousingTypes[housing.offer.type]);
-  addExistingTextContent(cardTemplate.querySelector('.popup__text--capacity'), `${housing.offer.rooms} комнаты для ${housing.offer.guests} гостей`);
-  addExistingTextContent(cardTemplate.querySelector('.popup__text--time'), `Заезд после ${housing.offer.checkin}, выезд до ${housing.offer.checkout}`);
-  addExistingTextContent(cardTemplate.querySelector('.popup__description'), housing.offer.description);
+  addExistingSource(cardTemplate.querySelector('.popup__avatar'), avatar);
+  addExistingTextContent(cardTemplate.querySelector('.popup__title'), title);
+  addExistingTextContent(cardTemplate.querySelector('.popup__text--address'), address);
+  addExistingTextContent(cardTemplate.querySelector('.popup__text--price'), `${price} ₽/ночь`);
+  addExistingTextContent(cardTemplate.querySelector('.popup__type'), HousingTypes[type]);
+  addExistingTextContent(cardTemplate.querySelector('.popup__text--capacity'), `${rooms} комнаты для ${guests} гостей`);
+  addExistingTextContent(cardTemplate.querySelector('.popup__text--time'), `Заезд после ${checkin}, выезд до ${checkout}`);
+  addExistingTextContent(cardTemplate.querySelector('.popup__description'), description);
 
   const popupFeaturesContainer = cardTemplate.querySelector('.popup__features');
   const featuresList = popupFeaturesContainer.querySelectorAll('.popup__feature');
-  const featuresClasses = housing.offer.features.map((feature) => `popup__feature--${feature}`);
+  const featuresClasses = features.map((feature) => `popup__feature--${feature}`);
 
   featuresList.forEach((featureItem) => {
     const featureClass = featureItem.classList[1];
@@ -30,7 +30,7 @@ const createPopup = (housing) => {
   const popupPhotosContainer = cardTemplate.querySelector('.popup__photos');
   const photoTemplate = popupPhotosContainer.querySelector('.popup__photo');
 
-  housing.offer.photos.forEach((photoSource) => {
+  photos.forEach((photoSource) => {
     const photoElement = photoTemplate.cloneNode();
     photoElement.src = photoSource;
     popupPhotosContainer.append(photoElement);
