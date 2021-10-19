68598aed63d4c4a716c591842e56820f76f92a28
Artur Samoilov <artsamoilov@gmail.com>
2021-10-19 22:31:52 +0300

    выносит общую функцию активации в utils

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -5,4 +5,9 @@ const getFloatFromRange = (min, max, precision) => {
 
 const getIntegerFromRange = (min, max) => getFloatFromRange(min, max, 0);
 
-export {getFloatFromRange, getIntegerFromRange};
+const setEnabled = (element, enabled, cssClass) => {
+  enabled ? element.classList.add(cssClass) : element.classList.remove(cssClass);
+  element.childNodes.forEach((child) => child.disabled = enabled);
+};
+
+export {getFloatFromRange, getIntegerFromRange, setEnabled};
