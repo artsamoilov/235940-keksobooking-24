86e8c5e47b80aebbc96dd8b5bc47d7532657e390
Artur Samoilov <artsamoilov@gmail.com>
2021-11-01 20:41:51 +0300

    исправляет показ и скрытие нотификаций

--- js/utils/notification.js
+++ js/utils/notification.js
@@ -1,16 +1,8 @@
 import {isEscKey} from './utils.js';
 
-const NOTIFICATION_SHOW_TIME = 3000;
-
-const hideNotification = () => {
-  if (document.querySelector('.success')) {
-    document.querySelector('.success').remove();
-  }
-  if (document.querySelector('.error')) {
-    document.querySelector('.error').remove();
-  }
-  document.removeEventListener('keydown', isEscKey);
-};
+const NOTIFICATION_SHOW_TIME = 30000;
+const successNotificationTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorNotificationTemplate = document.querySelector('#error').content.querySelector('.error');
 
 const onDocumentEscKeydown = (evt) => {
   if (isEscKey(evt)) {
@@ -19,22 +11,28 @@ const onDocumentEscKeydown = (evt) => {
   }
 };
 
+const onDocumentClick = () => hideNotification();
+
+function hideNotification () {
+  if (document.querySelector('.success')) {
+    document.querySelector('.success').remove();
+  }
+  if (document.querySelector('.error')) {
+    document.querySelector('.error').remove();
+  }
+  document.removeEventListener('click', onDocumentClick);
+  document.removeEventListener('keydown', onDocumentEscKeydown);
+}
+
 const showNotification = (notificationTemplate) => {
   document.body.append(notificationTemplate);
-  notificationTemplate.addEventListener('click', hideNotification);
+  document.addEventListener('click', onDocumentClick);
   document.addEventListener('keydown', onDocumentEscKeydown);
-
   setTimeout(hideNotification, NOTIFICATION_SHOW_TIME);
 };
 
-const showSuccessNotification = () => {
-  const successNotificationTemplate = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
-  showNotification(successNotificationTemplate);
-};
+const showSuccessNotification = () => showNotification(successNotificationTemplate.cloneNode(true));
 
-const showErrorNotification = () => {
-  const errorNotificationTemplate = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
-  showNotification(errorNotificationTemplate);
-};
+const showErrorNotification = () => showNotification(errorNotificationTemplate.cloneNode(true));
 
 export {showSuccessNotification, showErrorNotification};
