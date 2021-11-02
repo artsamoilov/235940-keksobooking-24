21f07479cd9c5f37e7aca64c3f7a822917dc1ee4
Artur Samoilov <artsamoilov@gmail.com>
2021-11-03 01:34:57 +0300

    добавляет сигнал для закрытия нотификаций

--- js/utils/notification.js
+++ js/utils/notification.js
@@ -3,6 +3,17 @@ import {isEscKey} from './utils.js';
 const NOTIFICATION_SHOW_TIME = 3000;
 const successNotificationTemplate = document.querySelector('#success').content.querySelector('.success');
 const errorNotificationTemplate = document.querySelector('#error').content.querySelector('.error');
+let controller = new AbortController();
+
+const hideNotification = () => {
+  if (document.querySelector('.success')) {
+    document.querySelector('.success').remove();
+  }
+  if (document.querySelector('.error')) {
+    document.querySelector('.error').remove();
+  }
+  controller.abort();
+};
 
 const onDocumentEscKeydown = (evt) => {
   if (isEscKey(evt)) {
@@ -13,21 +24,11 @@ const onDocumentEscKeydown = (evt) => {
 
 const onDocumentClick = () => hideNotification();
 
-function hideNotification () {
-  if (document.querySelector('.success')) {
-    document.querySelector('.success').remove();
-  }
-  if (document.querySelector('.error')) {
-    document.querySelector('.error').remove();
-  }
-  document.removeEventListener('click', onDocumentClick);
-  document.removeEventListener('keydown', onDocumentEscKeydown);
-}
-
 const showNotification = (notificationTemplate) => {
+  controller = new AbortController();
   document.body.append(notificationTemplate);
-  document.addEventListener('click', onDocumentClick);
-  document.addEventListener('keydown', onDocumentEscKeydown);
+  document.addEventListener('click', onDocumentClick, {signal: controller.signal});
+  document.addEventListener('keydown', onDocumentEscKeydown, {signal: controller.signal});
   setTimeout(hideNotification, NOTIFICATION_SHOW_TIME);
 };
 
